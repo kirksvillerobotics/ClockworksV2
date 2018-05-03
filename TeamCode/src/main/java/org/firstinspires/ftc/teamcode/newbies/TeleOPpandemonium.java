f7441ad18bd377a986f6eb87227f8f8c1ebc7e54
yearbook <caleb.andrew.flaim@gmail.com>
2018-05-03 16:21:42 -0500

    more of the same

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newbies/TeleOPpandemonium.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newbies/TeleOPpandemonium.java
@@ -3,20 +3,23 @@ package org.firstinspires.ftc.teamcode.newbies;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by yearbook on 5/2/18.
- */
+ **/
 
-@TeleOp (name = "TeleOp pandemonium")
+@TeleOp (name = "TeleOp Pandemonium")
 public class TeleOPpandemonium extends OpMode {
 
-    public DcMotor leftDrive;
-    public DcMotor rightDrive;
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
 
-    public DcMotor armMove;
+    private DcMotor armMove;
 
-    public final double THRESHOLD = 0.05;
+
+    private Servo rightMove;
+    private Servo leftMove;
 
     @Override
     public void init() {
@@ -25,16 +28,20 @@ public class TeleOPpandemonium extends OpMode {
 
         armMove = hardwareMap.dcMotor.get("armMove");
 
+        rightMove = hardwareMap.servo.get("rightMove");
+        leftMove = hardwareMap.servo.get("leftMove");
     }
 
     @Override
     public void loop() {
+        final double THRESHOLD = 0.05;
+
         if (gamepad1.left_stick_y > THRESHOLD) {  // 0.0 - 1.0
             leftDrive.setPower(1.0); // 0.0 - 1.0
             telemetry.addData("Left Motor", "On");
         } else {
             leftDrive.setPower(0.0);
-            telemetry.addData("Right Motor", "Off");
+            telemetry.addData("Left Motor", "Off");
         }
 
         if (gamepad1.right_stick_y > THRESHOLD) { // 0.0 - 1.0
@@ -44,7 +51,26 @@ public class TeleOPpandemonium extends OpMode {
             rightDrive.setPower(0.0);
             telemetry.addData("Right Motor", "Off");
         }
-        armMove.setPower(gamepad2.right_trigger);
-        armMove.setPower(-gamepad2.left_trigger);
+        armMove.setPower(gamepad2.right_trigger); //up?
+        armMove.setPower(-gamepad2.left_trigger); //down?
+
+        if (gamepad2.right_stick_x > THRESHOLD) {
+            rightMove.setPosition(1.00);
+        } else {
+            if (gamepad2.right_stick_x < THRESHOLD) {
+                rightMove.setPosition(0.00);
+            } else {
+                rightMove.setPosition(0.50);
+            }
+        }
+        if (gamepad2.left_stick_x > THRESHOLD) {
+            leftMove.setPosition(1.00);
+        } else {
+            if (gamepad2.left_stick_x < THRESHOLD) {
+                leftMove.setPosition(0.00);
+            } else {
+                leftMove.setPosition(0.50);
+            }
+        }
     }
 }
